338d76d236dcc16a7ab834a8c81e95e0247c5326
Anvesh <anvthak@gmail.com>
2021-09-24 16:10:50 +0530

    After the data generation and interaction.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { useState } from 'react';
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import idl from './idl.json';
-
 import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
 import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import norse from './Norse-Data.json';
 
 const wallets = [ getPhantomWallet() ]
-
 const { SystemProgram, Keypair } = web3;
 const baseAccount = Keypair.generate();
 const opts = {
   preflightCommitment: "processed"
 }
+
 const programID = new PublicKey(idl.metadata.address);
 const network = clusterApiUrl("devnet");
 
@@ -22,13 +22,13 @@ function App() {
   const [value, setValue] = useState('');
   const [dataList, setDataList] = useState([]);
   const [input, setInput] = useState('');
+
   const wallet = useWallet()
 
   async function getProvider() {
     /* create the provider and return it to the caller */
     /* network set to local network for now */
     const connection = new Connection(network, opts.preflightCommitment);
-
     const provider = new Provider(
       connection, wallet, opts.preflightCommitment,
     );
@@ -37,8 +37,10 @@ function App() {
 
   async function initialize() {    
     const provider = await getProvider();
+    
     /* create the program interface combining the idl, program ID, and provider */
     const program = new Program(idl, programID, provider);
+    
     try {
       /* interact with the program via rpc */
       await program.rpc.initialize("Hello World", {
@@ -62,7 +64,9 @@ function App() {
   async function update() {
     if (!input) return
     const provider = await getProvider();
+    
     const program = new Program(idl, programID, provider);
+    
     await program.rpc.update(input, {
       accounts: {
         baseAccount: baseAccount.publicKey
@@ -76,11 +80,41 @@ function App() {
     setInput('');
   }
 
+
+  //Making the NORSE GODS and Minting the 
+  const norseGodHandler = async() => {
+    if(dataList.length < 3){
+      await update();
+    }
+    else{
+      console.log("Already Have 3 Cards it's time to play")
+      console.log(dataList);
+    }
+  }
+
+  const createCombatCards = async() => {
+    if(dataList < 3) console.log("You Should Make Some Cards First");
+    
+    let userCardsObject = {
+      user: wallet.publicKey.toString(),
+      cards: [
+        norse[dataList[0]],
+        norse[dataList[1]],
+        norse[dataList[2]],
+      ]
+    }
+    console.log(userCardsObject);
+  }
+
+
+
   if (!wallet.connected) {
     return (
+      <>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
         <WalletMultiButton />
       </div>
+      </>
     )
   } else {
     return (
@@ -89,30 +123,47 @@ function App() {
           {
             !value && (<button onClick={initialize}>Initialize</button>)
           }
-
           {
             value ? (
               <div>
-                <h2>Current value: {value}</h2>
                 <input
-                  placeholder="Add new data"
+                  placeholder="Your Random User Seed"
                   onChange={e => setInput(e.target.value)}
                   value={input}
                 />
-                <button onClick={update}>Add data</button>
+                <br />
+                <br />
+                <button onClick={norseGodHandler}>Pick NFT</button>
+                <br />
+                <br />
+                <button onClick={createCombatCards}>Create Combat Cards</button>
               </div>
+            
             ) : (
-              <h3>Please Inialize.</h3>
+              <h3>Please Initialize.</h3>
             )
           }
+          <br />
           {
-            dataList.map((d, i) => <h4 key={i}>{d}</h4>)
+            dataList.map((d, i) => 
+            <h4 key={i}> 
+              <ul style = {{listStyle: 'none'}}>
+                <li>Name: {norse[d].name}</li> 
+                <li>Level: {norse[d].Level}</li>
+                <li>Hp: {norse[d].HP}</li>
+                <li>Strength: {norse[d].Strength}</li>
+              </ul>
+            </h4>
+            )
           }
         </div>
       </div>
     );
   }
 }
+
+
+
 
 const AppWithProvider = () => (
   <ConnectionProvider endpoint={network}>
@@ -125,3 +176,4 @@ const AppWithProvider = () => (
 )
 
 export default AppWithProvider;
+// 0.00268 + 0.00001
